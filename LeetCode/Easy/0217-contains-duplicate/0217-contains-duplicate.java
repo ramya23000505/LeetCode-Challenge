3e8cf35409837ca2ebb67f4ea6465c64fe5f355c
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2026-03-13 14:49:55 +0530

    .java 0217-contains-duplicate Array 03-13-2026

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-               return true;
-            }
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
         }
         return false;
     }
